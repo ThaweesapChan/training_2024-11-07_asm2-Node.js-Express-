cd868e79a7cdf59eeaa5f0f26891ef634a7ad1ca
thaweesap chantasathanont <thaweesap@192.168.1.35>
2024-11-07 14:52:14 +0700

    feat: test API

---
+++ app.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+app.get("/profiles", (req, res) => {
+  return res.status(200).json({
+    data: {
+      name: "john",
+      age: 20,
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`running ar ${port}`);
+});
